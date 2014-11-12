45e13155e82b32a816cb50ca31861e57b6a59716
Lavender <moonycat98@gmail.com>
2014-11-13 01:15:55 +0400

    Add hide() function

--- plugins/schedule.py
+++ plugins/schedule.py
@@ -16,10 +16,17 @@ class Plugin(BasePlugin):
         self.gui = gui
         self.win = self.gui.main_window
         self.add_time_edit()
+        self.win.update_status()
+
+    def close(self):
+        self.lable_time.hide()
+        self.time_e.hide()
+        self.win.update_status()
 
     def add_time_edit(self):
         #self.time_e = QDateTimeEdit().setDateTime(QDateTime.currentDateTime())
+        self.lable_time = QLabel('Time')
         self.time_e = QDateTimeEdit()
-        self.win.send_grid.addWidget(QLabel('Time'), 5, 0)
+        self.win.send_grid.addWidget(self.lable_time, 5, 0)
         self.win.send_grid.addWidget(self.time_e, 5, 1, Qt.AlignHCenter)
 
