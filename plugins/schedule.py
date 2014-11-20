e9b4789b48b74ee8ebce4cd3524751d47fc4700d
Lavender <moonycat98@gmail.com>
2014-11-21 02:06:44 +0400

    Change to QTimer version

--- plugins/schedule.py
+++ plugins/schedule.py
@@ -1,9 +1,15 @@
 from PyQt4.QtGui import *
 from PyQt4.QtCore import *
+from PyQt4 import QtSql
 
 from electrum.i18n import _, set_language
+from electrum import SimpleConfig, Transaction
+from electrum.bitcoin import MIN_RELAY_TX_FEE
 from electrum.plugins import BasePlugin, hook
-from gui.qt.util import HelpButton, EnterButton, MyTreeWidget
+from gui.qt.util import HelpButton, EnterButton
+
+import sqlite3, time, functools
+import sys, traceback
 
 class Plugin(BasePlugin):
 
@@ -25,8 +31,8 @@ class Plugin(BasePlugin):
         self.win.payto_sig.hide()
         self.win.send_grid.itemAtPosition(6, 2).widget().hide()
         self.add_time_edit()
-        self.add_schedule_widget()
-        self.update_schedule_widget()
+        self.add_schedule_table()
+        self.check_schedule()
         self.win.update_status()
 
     def close(self):
@@ -38,7 +44,7 @@ class Plugin(BasePlugin):
         self.new_send_button.hide()
         self.new_clear_button.hide()
         self.label_request.hide()
-        self.schedule_list.hide()
+        self.view.hide()
         self.win.send_button.show()
         self.win.payto_sig.show()
         self.win.send_grid.itemAtPosition(6, 2).widget().show()
@@ -53,7 +59,7 @@ class Plugin(BasePlugin):
         self.group.addButton(self.schedule_r) 
         self.instant_r.setChecked(True)
         self.time_e = QDateTimeEdit()
-        self.time_e.setMinimumDateTime(QDateTime.currentDateTime().addSecs(300))
+        self.time_e.setMinimumDateTime(QDateTime.currentDateTime())
         self.time_help = HelpButton(_('Schedule a transaction.') + '\n\n' + _('Set time for a transaction.'))
         self.new_send_button = EnterButton(_('Send'), self.read_send_tab)
         self.new_clear_button = EnterButton(_('Clear'), self.win.do_clear)
@@ -66,23 +72,63 @@ class Plugin(BasePlugin):
         self.win.send_grid.addWidget(self.new_send_button, 7, 1, Qt.AlignLeft)
         self.win.send_grid.addWidget(self.new_clear_button, 7, 2, Qt.AlignLeft)
 
-    def add_schedule_widget(self):
+    def add_schedule_table(self):
         self.win.send_grid.setRowStretch(8, 1)
-        self.label_request = QLabel(_('Saved Schedule'))
-        self.schedule_list = MyTreeWidget(None)
-        self.schedule_list.setColumnCount(4)
-        self.schedule_list.customContextMenuRequested.connect(self.schedule_list_menu)
-        self.schedule_list.setHeaderLabels( [_('Date'), _('Amount'), _('Fee'), ('Address')] )
-        self.schedule_list.setColumnWidth(0, 150)
-        self.schedule_list.setColumnWidth(1, 150)
-        self.schedule_list.setColumnWidth(2, 150)
 
-        self.win.send_grid.addWidget(self.label_request, 9, 0)
-        self.win.send_grid.addWidget(self.schedule_list, 10, 0, 1, 6)
+        conn = sqlite3.connect('/tmp/schedule.db')
+        c = conn.cursor()
+        c.execute("CREATE TABLE IF NOT EXISTS list (n INTEGER PRIMARY KEY, timestamp INTEGER, datetime TEXT, amount INTEGER, fee INTEGER, address TEXT);")
+        conn.commit()
+        conn.close()
+
+        self.label_request = QLabel(_('Saved Schedule'))
+        db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
+        db.setDatabaseName("/tmp/schedule.db")
+        db.open()
+        model = QtSql.QSqlTableModel(self.win.send_grid, db)
+        self.initialize_model(model)
+        self.view = self.create_view(model)
+        db.close()
+        QtSql.QSqlDatabase.removeDatabase(db.connectionName())
+
+        self.win.send_grid.addWidget(self.label_request, 8, 0)
+        self.win.send_grid.addWidget(self.view, 9, 0, 1, 6)
+
+    def check_schedule(self):
+        conn = sqlite3.connect('/tmp/schedule.db')
+        c = conn.cursor()
+        self.i = 0
+        self.timer = []
+
+        for row in list(c.execute('SELECT * FROM list')):
+                t = row[1] - time.time()
+                if t < 0:
+                    if not self.win.question("A transaction to '" + str(row[5]) + "' with amount " + str(row [3]) + " (satoshis) has expired.\n\nDo you want to send it now?\n\nIf you choose 'No', the transaction will be deleted."):
+                        c.execute('DELETE FROM list WHERE n=(?)', (row[0],))
+                    else:
+                        self.timer.append(QTimer())
+                        timerCallback = functools.partial(self.onTimer, row[1], row[3], row[4], row[5])
+                        self.timer[self.i].singleShot(60000, timerCallback)
+                        self.i = self.i + 1
+                else:
+                    self.timer.append(QTimer())
+                    timerCallback = functools.partial(self.onTimer, row[1], row[3], row[4], row[5])
+                    self.timer[self.i].singleShot(int(t)*1000, timerCallback)
+                    print len(self.timer), t, self.i
+                    self.i = self.i + 1
+
+        conn.commit()
+        conn.close()
+        self.label_request.hide()
+        self.view.hide()
+        self.add_schedule_table()
+
+    def onTimer(self, n, amount, fee, addr):
+        self.do_send(n, amount, fee, addr)
 
     def read_send_tab(self):
         if self.instant_r.isChecked():
-            #self.win.do_send()
+            self.win.do_send()
             return
         elif self.schedule_r.isChecked():
             self.do_schedule()
@@ -92,38 +138,92 @@ class Plugin(BasePlugin):
 
     def do_schedule(self):
         r = self.win.read_send_tab()
-        now = QDateTime.currentDateTime()
-        t = now.secsTo(self.time_e.dateTime())
         if not r:
             return
-        if t <= 240:
-            QMessageBox.warning(None, _('Error'), _("Please select a time at least five minutes later than current time"), _('OK'))
+
+        now = QDateTime.currentDateTime()
+        t = self.time_e.dateTime()
+        if t < now:
+            QMessageBox.warning(None, _('Error'), "The time you selected has passed.\n\nPlease modify the time before re-scheduling.", _('OK'))
             return
-        print t
-        time = self.time_e.dateTime().toTime_t()
+
+        time_stamp = t.toTime_t()
+        time_datetime = t.toPyDateTime().strftime("%Y-%m-%d %H:%M:%S")
         amount = self.win.amount_e.get_amount()
         fee = self.win.fee_e.get_amount()
         addr = self.win.payto_e.get_outputs()[0][1]
 
-        self.schedule_requests = self.wallet.storage.get('schedule_requests',{}) 
-        self.schedule_requests[time] = (amount, fee, addr)
-        self.wallet.storage.put('schedule_requests', self.schedule_requests)
-        self.update_schedule_widget()
+        conn = sqlite3.connect('/tmp/schedule.db')
+        c = conn.cursor()
+        c.execute("INSERT INTO list VALUES (?, ?, ?, ?, ?, ?);", (None, time_stamp, time_datetime, amount, fee, addr))
+        conn.commit()
+        conn.close()
 
-    def schedule_list_menu(self, position):
-        item = self.schedule_list.itemAt(position)
-        menu = QMenu()
-        #menu.addAction(_("Delete"), lambda: self.schedule_list_delete(item))
-        menu.exec_(self.schedule_list.viewport().mapToGlobal(position))
+        self.timer.append(QTimer())
+        timerCallback = functools.partial(self.onTimer, time_stamp, amount, fee, addr)
+        time_sec = time_stamp- time.time()
+        self.timer[self.i].singleShot(int(time_sec)*1000, timerCallback)
+        self.i = self.i + 1
 
-    def update_schedule_widget(self):
-        b = len(self.schedule_requests) > 0
-        self.schedule_list.setVisible(b)
+        self.label_request.hide()
+        self.view.hide()
+        self.add_schedule_table()
 
-        self.schedule_list.clear()
-        for time, v in self.schedule_requests.items():
-            amount, fee, addr = v
-            item = QTreeWidgetItem( [ time, self.win.format_amount(amount) if amount else "", self.win.format_amount(fee) if amount else "", addr])
-            item.setFont(0, QFont(MONOSPACE_FONT))
-            self.schedule_list.addTopLevelItem(item)
+    def do_send(self, time_stamp, amount, fee, addr):
+        outputs = [('address', str(addr), amount)]
+        label = ''
+        coins = self.win.get_coins()
 
+        try:
+            tx = self.wallet.make_unsigned_transaction(outputs, fee, None, coins = coins)
+            tx.error = None
+        except Exception as e:
+            traceback.print_exc(file=sys.stdout)
+            self.win.show_message(str(e))
+            return
+
+        if tx.requires_fee(self.wallet.verifier) and tx.get_fee() < MIN_RELAY_TX_FEE:
+            QMessageBox.warning(self, _('Error'), _("This transaction requires a higher fee, or it will not be propagated by the network."), _('OK'))
+            return
+
+        if not self.config.get('can_edit_fees', False):
+            if not self.win.question(_("A fee of %(fee)s will be added to this transaction.\nProceed?")%{ 'fee' : self.win.format_amount(fee) + ' '+ self.win.base_unit()}):
+                return
+        else:
+            confirm_fee = self.config.get('confirm_fee', 100000)
+            if fee >= confirm_fee:
+                if not self.win.question(_("The fee for this transaction seems unusually high.\nAre you really sure you want to pay %(fee)s in fees?")%{ 'fee' : self.win.format_amount(fee) + ' '+ self.win.base_unit()}):
+                    return
+
+        self.win.send_tx(tx, label)
+
+        conn = sqlite3.connect('/tmp/schedule.db')
+        c = conn.cursor()
+        c.execute('DELETE FROM list WHERE timestamp=(?)', (time_stamp,))
+        conn.commit()
+        conn.close()
+        self.label_request.hide()
+        self.view.hide()
+        self.add_schedule_table()
+
+    def initialize_model(self, model):
+        model.setTable("list")
+        model.setEditStrategy(QtSql.QSqlTableModel.OnManualSubmit)
+        model.select()
+        model.setHeaderData(0, Qt.Horizontal, "")
+        model.setHeaderData(1, Qt.Horizontal, "Timestamp")
+        model.setHeaderData(2, Qt.Horizontal, "Date")
+        model.setHeaderData(3, Qt.Horizontal, "Amount (Satoshis)")
+        model.setHeaderData(4, Qt.Horizontal, "Fee (Satoshis)")
+        model.setHeaderData(5, Qt.Horizontal, "Address")
+
+    def create_view(self, model):
+        view = QTableView()
+        view.setModel(model)
+        view.hideColumn(0)
+        view.hideColumn(1)
+        view.setColumnWidth(2, 170)
+        view.setColumnWidth(3, 150)
+        view.setColumnWidth(4, 150)
+        view.horizontalHeader().setStretchLastSection(True)
+        return view
